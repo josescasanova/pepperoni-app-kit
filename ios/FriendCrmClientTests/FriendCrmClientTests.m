5c330f59e2f59729d8485170725fd208c9954fb4
Jose Casanova <josescasanova@gmail.com>
2016-07-02 22:29:21 -0400

    commit

--- ios/FriendCrmClientTests/FriendCrmClientTests.m
+++ ios/FriendCrmClientTests/FriendCrmClientTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface FriendCrmClientTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation FriendCrmClientTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
